be4f6f4f08ddcdc08688da52d5f85cf9aceef682
francisco <franciscoantunesbarreto1997@gmail.com>
2020-04-10 13:29:07 +0100

    front-end was way too messy and didn't perform as expected, starting from scratch

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,3 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
-
-import carousel from '../functions/carousel';
-import responsiveNavbar from '../functions/responsive_navbar'
-
-carousel();
-responsiveNavbar();
