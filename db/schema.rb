0f3b122117a4c668a158b9fa04af85d30ecc312c
francisco <franciscoantunesbarreto1997@gmail.com>
2020-04-11 20:48:06 +0100

    added several attributes to the user model and corresponding validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_170643) do
+ActiveRecord::Schema.define(version: 2020_04_11_183807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,8 +70,13 @@ ActiveRecord::Schema.define(version: 2020_04_11_170643) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.bigint "school_class_id", null: false
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["school_class_id"], name: "index_users_on_school_class_id"
   end
 
   create_table "users_subjects", id: false, force: :cascade do |t|
@@ -87,4 +92,5 @@ ActiveRecord::Schema.define(version: 2020_04_11_170643) do
   add_foreign_key "school_classes", "schools"
   add_foreign_key "subjects", "school_classes"
   add_foreign_key "subjects", "users"
+  add_foreign_key "users", "school_classes"
 end
