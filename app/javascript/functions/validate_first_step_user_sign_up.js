1c7e07e5fc879c5cf4c9241bdf26b14900990375
francisco <franciscoantunesbarreto1997@gmail.com>
2020-04-13 14:05:30 +0100

    validating form inputs has the user writes them instead of on submit

--- app/javascript/functions/validate_first_step_user_sign_up.js
+++ app/javascript/functions/validate_first_step_user_sign_up.js
@@ -3,18 +3,59 @@ const validateFirstStepUserSignUp = () => {
   const formGroups = document.querySelectorAll('form#new_user .form-inputs .form-group')
   let validations = 0;
 
+  validateInputs(formGroups);
+
   if (nextButton) {
     nextButton.addEventListener('click', (event) => {
       event.preventDefault();
-      validateInputs(formGroups);
+      // validateInputs(formGroups);
     })
   }
 }
 
 const validateInputs = (formGroups) => {
   formGroups.forEach((group) => {
-    console.log(group.firstChild.value)
+    group.firstChild.addEventListener('blur', (event) => {
+      const groupId = group.firstChild.id;
+      const userInput = group.firstChild.value;
+
+      switch(group.firstChild.id) {
+        case "user_email":
+        validateEmail(userInput);
+        break;
+        case "user_first_name":
+        validateName(userInput);
+        break;
+        case "user_last_name":
+        validateName(userInput);
+        break;
+        case "user_password":
+        validatePassword(userInput);
+        break;
+        case "user_password_confirmation":
+        validatePasswordConfirmation(userInput);
+        break;
+        default:
+        break;
+      }
+    })
   })
+}
+
+const validateEmail = (input) => {
+
+}
+
+const validateName = (input) => {
+
+}
+
+const validatePassword = (input) => {
+
+}
+
+const validatePasswordConfirmation = (input) => {
+
 }
 
 export default validateFirstStepUserSignUp;
