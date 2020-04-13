422c7dcae73d9aa209f2ec685c2e6b2314268f61
francisco <franciscoantunesbarreto1997@gmail.com>
2020-04-13 11:59:09 +0100

    added school code to users for seamless account creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_175758) do
+ActiveRecord::Schema.define(version: 2020_04_13_105703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2020_04_12_175758) do
     t.string "last_name"
     t.bigint "school_class_id", null: false
     t.bigint "role_id", null: false
+    t.string "school_code"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
