53bf072d599d5ec12b1c3bf8e8807c0e990a289c
francisco <franciscoantunesbarreto1997@gmail.com>
2020-04-19 16:51:25 +0100

    navbar fully responsive

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,7 +1,4 @@
 class RolesController < ApplicationController
-
-  skip_before_action :authenticate_user!, only: [:fetch_roles_for_sign_up]
-
   def fetch_roles_for_sign_up
     render json: Role.all
   end
