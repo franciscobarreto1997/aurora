8b87fadde72584a5d05a91df65afdcc190fb31e0
francisco <franciscoantunesbarreto1997@gmail.com>
2020-04-19 15:48:57 +0100

    started adding the navbar menu, user now gets redirected to the dashboard if its already signed in

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ import removeCheckboxLabel from '../functions/remove_checkbox_label_sign_up_form
 import selectRoleButton from '../functions/select_role_button';
 import validateFirstStepUserSignUp from '../functions/validate_first_step_user_sign_up';
 import validateSchoolAdminSignUp from '../functions/validate_school_admin_sign_up';
+import toggleNavbarMenu from '../functions/toggle_navbar_menu';
 
 
 responsiveLandingPageNavbar();
@@ -20,6 +21,7 @@ removeCheckboxLabel();
 selectRoleButton();
 validateFirstStepUserSignUp();
 validateSchoolAdminSignUp();
+toggleNavbarMenu();
 
 window.addEventListener('resize', () => {
   animeLandingPage();
