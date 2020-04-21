bbfc12c7001e884880522cfd12e078ed360b4d44
francisco <franciscoantunesbarreto1997@gmail.com>
2020-04-21 21:12:25 +0100

    at last, solved this fucking turbolinks issue with rails 6, started using old rails 5 way of handling js in the asset pipeline, moved some requirements from the packs in app/javascript to assets/javascripts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   def home
-    redirect_to new_school_path if school_admin_signed_in?
+    if school_admin_signed_in?
+      school = School.find_by school_admin_id: current_school_admin.id
+      redirect_to school_school_classes_path(school.id)
+    end
     redirect_to subjects_path if user_signed_in?
   end
 end
